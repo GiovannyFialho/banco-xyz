352a5fff5e2d49f326f46d5201f3c9e6b80075be
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-01-17 14:36:17 -0300

    adicionando fallback no carregamento da pagina inicial

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,16 +1,30 @@
-import { useEffect } from "react";
+import { LoaderCircle } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router";
 
 export function AppLayout() {
   const navigate = useNavigate();
-
-  const userToken = localStorage.getItem("bankXYZ@user-token");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const userToken = localStorage.getItem("bankXYZ@user-token");
+
     if (!userToken) {
       navigate("/sign-in", { replace: true });
+    } else {
+      setLoading(false);
     }
-  }, [userToken, navigate]);
+  }, [navigate]);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div className="h-max w-max animate-spin">
+          <LoaderCircle size={50} className="text-green-600" />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex min-h-screen flex-col antialiased">
